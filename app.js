b64b5c2dbb433884d3e9c72e45ac2dc60c984483
FrancisCKY <a231685600@gmail.com>
2023-08-19 23:55:38 +0800

    page initialization

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
 
+const router = require('./routes')
+
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
+
+app.use(router)
 
 app.get('/', (req, res) => {
   res.render('index')
