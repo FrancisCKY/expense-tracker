c34600a6c6e9b266f6cbefb79c49ec487889a987
FrancisCKY <a231685600@gmail.com>
2023-08-29 11:26:06 +0800

    add record-category relationship migrations

--- models/record.js
+++ models/record.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Record.belongsTo(models.Category, {
+        foreignKey: 'categoryId'
+      })
     }
   }
   Record.init({
